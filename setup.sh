da6de75c67421fef756950d587ef8b3d8b18e7f6
ysnbhb <yassine.bahbib@usmba.ac.ma>
2025-07-06 00:03:16 +0100

    good

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 setting() {
+    # Download wallpaper as webp
+    wget -q https://4kwallpapers.com/images/walls/thumbs_2t/12111.png -O "$HOME/Pictures/wallpaper.webp"
+
+    # Convert webp to jpg for better GNOME compatibility
+    convert "$HOME/Pictures/wallpaper.webp" "$HOME/Pictures/wallpaper.jpg"
+
+    # Set wallpaper URI using the jpg version
+    gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/wallpaper.jpg"
+
+    # Optionally for dark mode too
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/Pictures/wallpaper.jpg"
+
+    # Other settings
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
     gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
     gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
     gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-
 }
 
 store_git() {
@@ -14,7 +26,8 @@ store_git() {
     git config --global user.email "yassine.bahbib@usmba.ac.ma"
 }
 
-setup_exntion() {
+setup_extension() {
+    # Install VSCode extensions silently and in background
     code --install-extension foxundermoon.shell-format >/dev/null 2>&1 &
     code --install-extension ritwickdey.LiveServer >/dev/null 2>&1 &
     code --install-extension golang.go >/dev/null 2>&1 &
@@ -30,17 +43,25 @@ setup_exntion() {
 }
 
 open_app() {
-    discord &
+    # Uncomment discord if you want it opened
+    # discord >/dev/null 2>&1 &
     xdg-open "https://github.com" >/dev/null 2>&1 &
     xdg-open "https://chatgpt.com/" >/dev/null 2>&1 &
     xdg-open "https://learn.zone01oujda.ma/intra/oujda" >/dev/null 2>&1 &
     xdg-open "https://web.telegram.org/k/" >/dev/null 2>&1 &
 }
-setting
-# echo bash >>~/.zshrc
-echo "source $HOME/.shell" >>~/.bashrc
 
-cp .shell $HOME
-setup_exntion
+# Run the functions
+
+# Add source line to ~/.bashrc only if not already present
+if ! grep -qxF "source \$HOME/.shell" "$HOME/.bashrc"; then
+    echo "source \$HOME/.shell" >>"$HOME/.bashrc"
+fi
+
+# Copy the .shell file to home directory
+cp .shell "$HOME/"
+
+setting
+setup_extension
 open_app
 store_git
